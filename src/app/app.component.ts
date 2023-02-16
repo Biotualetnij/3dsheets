af5a96f9c73a985d8247119228ee06c9f5a39f93
Biotualetnij <72648074+Biotualetnij@users.noreply.github.com>
2023-02-16 09:55:18 +0200

    added features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  zSpacing = -1000;
+  lastPos = this.zSpacing / 5;
+  $frames: any = null;
+  frames: any;
+  zVals: Array<any> = [];
   title = '3d-sheet';
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.$frames = document.querySelectorAll('.frame');
+    console.log(this.$frames);
+    this.frames = Array.from(this.$frames);
+
+    window.onscroll = () => {
+      let top = document.documentElement.scrollTop;
+      let delta = this.lastPos - top;
+      this.lastPos = top;
+      this.frames.forEach((element: any, i: number) => {
+        this.zVals.push(i * this.zSpacing + this.zSpacing);
+        this.zVals[i] += delta * -5;
+        let frame = this.frames[i],
+          transform = `translateZ(${this.zVals[i]}px)`,
+          opacity = this.zVals[i] < Math.abs(this.zSpacing) / 1.5 ? 1 : 0;
+        frame.setAttribute(
+          'style',
+          `transform:${transform}; opacity:${opacity}`
+        );
+      });
+    };
+    window.scrollTo(0, 20);
+  }
 }
